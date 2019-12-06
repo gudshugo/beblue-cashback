6d5b0d45837a18bca64a14544a7a451364893097
gudshugo <gudshugo@gmail.com>
2019-12-06 18:33:59 -0300

    C - Primeiro commit com estrutura do projeto em Spring

---
+++ src/main/java/br/com/project/rest/cashback/CashbackApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.rest.cashback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashbackApplication.class, args);
+	}
+
+}
