17d192d538618b39967b731c782a2f5785ffddcf
Hugo Gois <gudshugo@gmail.com>
2020-02-05 08:57:21 -0300

    Update stop-application.sh

--- stop-application.sh
+++ stop-application.sh
@@ -4,12 +4,12 @@
 echo "CASHBACK API BEBLUE V-0.0.1"
 echo "Projeto em: Spring Framework"
 echo "Autor: Hugo Gois Santos"
-echo "Projeto de uma API REST em Java (Spring Framework) referente ao teste técnico para desenvolvedor java na Fintech Beblue."
+echo "Projeto de uma API REST em Java (Spring Framework)."
 
 
 
 echo "Derrubando container..."
-sudo docker stop cashback-api;
-
+sudo docker stop cashback-api
+sudo docker rm cashback-api
 
 echo "Aplicação offline..."
